033114b52bde48ddd32d0289c90f07217f07c054
rafaelalma <rafaelalma92@gmail.com>
2021-02-14 12:26:39 +0100

    Implement main activity: dice rolling

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
+// This activity allows the user to roll two dices and view the results on the screen.
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.button)
+        rollButton.setOnClickListener {
+            rollDice(6, R.id.textView)
+            rollDice(2, R.id.textView2)
+        }
+    }
+
+    private fun rollDice (numSides: Int, id: Int) {
+        val dice = Dice(numSides)
+        val diceRoll = dice.roll()
+
+        updateTextView(id, diceRoll)
+    }
+
+    private fun updateTextView (id: Int, diceRoll: Int) {
+        val resultTextView: TextView = findViewById(id)
+        resultTextView.text = diceRoll.toString()
+    }
+}
+
+// This class defines a dice with a number of sides and a roll behaviour.
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
