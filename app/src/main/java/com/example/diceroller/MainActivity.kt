041c756bef69dcc877e019e63bf37dbc537f2e9f
rafaelalma <rafaelalma92@gmail.com>
2021-02-14 17:06:39 +0100

    Two dices implemented

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -5,34 +5,33 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 
-// This activity allows the user to roll one dice and view the result on the screen.
+// This activity allows the user to roll two dices and view the result on the screen.
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener { rollDice(6) }
+        val diceLeftImageView: ImageView = findViewById(R.id.diceLeftImageView)
+        val diceRightImageView: ImageView = findViewById(R.id.diceRightImageView)
 
-        // Do a dice roll when the app starts.
-        rollDice(6)
+        val rollButton: Button = findViewById(R.id.rollButton)
+        rollButton.setOnClickListener {
+            updateImageView(rollDice(6), diceLeftImageView)
+            updateImageView(rollDice(6), diceRightImageView)
+        }
+
+        // Roll two dices when the app starts.
+        updateImageView(rollDice(6), diceLeftImageView)
+        updateImageView(rollDice(6), diceRightImageView)
     }
 
-    // Roll one dice.
-    private fun rollDice(numSides: Int) {
+    private fun rollDice(numSides: Int): Int {
 
-        // Create a Dice object with 6 sides and roll it.
         val dice = Dice(numSides)
-        val diceRoll = dice.roll()
-
-        updateImageView(diceRoll)
+        return dice.roll()
     }
 
-    // Update the screen with the result.
-    private fun updateImageView(diceRoll: Int) {
-
-        // Find the ImageView in the layout.
-        val diceImage: ImageView = findViewById(R.id.imageView)
+    private fun updateImageView(diceRoll: Int, diceImage: ImageView) {
 
         // Determine which drawable resource ID to use based on the dice roll.
         val drawableResource = when(diceRoll) {
@@ -44,7 +43,6 @@ class MainActivity : AppCompatActivity() {
             else -> R.drawable.dice_6
         }
 
-        // Update the Image View and its content description.
         diceImage.setImageResource(drawableResource)
         diceImage.contentDescription = diceRoll.toString()
     }
